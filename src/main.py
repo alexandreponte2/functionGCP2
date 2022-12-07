27cb1f46bda5776ee6e7b3ba90fb85c786f42499
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-12-07 15:27:23 -0300

    add function test

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import base64
+import json
+import re
+from os import getenv
+
+
+PROJECT_ID = getenv("PROJECT_ID")
+
+def echo_teste():
+ print (PROJECT_ID)
+ payload = PROJECT_ID
+ return payload
