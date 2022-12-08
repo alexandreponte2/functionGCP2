bc1506072076cd127a963ce13e70fa8f56ff94d2
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-12-08 11:53:32 -0300

    add url token

--- src/main.py
+++ src/main.py
@@ -1,22 +1,62 @@
+from requests import get
 import base64
 import json
 import re
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+from google.cloud import secretmanager
+from random import choice
+import string
 from os import getenv
 
 
-PROJECT_ID = getenv("PROJECT_ID")
-SECRET_ID  = getenv("SECRET_ID")
+###Filtrar os dados:
+#PROJECT_ID = getenv("PROJECT_ID")
 
-def echo_teste():
- print (PROJECT_ID)
- payload = PROJECT_ID
- return payload
+TOKEN = getenv("TOKEN")
+ENVIROMENT = getenv("ENVIROMENT")
+PRODUCT = getenv("PRODUCT")
+URL_API = getenv("URL_API")
+HEADERS = {'Authorization': (TOKEN)}
+
+def get_secret_list():
+    r = get(
+        f'{URL_API}/{PRODUCT}/{ENVIROMENT}/?refresh=1', headers=HEADERS)
+    return r.json()
+
+
+apireturn =  get_secret_list()
+
+get_secret_list()
+
+print(apireturn)
 
 
 
-senha = echo_teste()
-print(format(senha))
 
-secret = SECRET_ID
+# old
 
-print(secret)
+
+
+# import base64
+# import json
+# import re
+# from os import getenv
+
+
+# PROJECT_ID = getenv("PROJECT_ID")
+# SECRET_ID  = getenv("SECRET_ID")
+
+# def echo_teste():
+#  print (PROJECT_ID)
+#  payload = PROJECT_ID
+#  return payload
+
+
+
+# senha = echo_teste()
+# print(format(senha))
+
+# secret = SECRET_ID
+
+# print(secret)
